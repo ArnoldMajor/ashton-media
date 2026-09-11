e5c563c009b17447d061369ec3c59aa43686fa3e
ArnoldMajor <arnoldmwangakala@gmail.com>
2026-09-11 20:17:34 +0300

    Detailed map updates

--- public-site/app/layout.tsx
+++ public-site/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning>
         <Nav />
         <main>{children}</main>
         <Footer />
